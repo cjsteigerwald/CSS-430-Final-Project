b961c2994f8d3de65dea279b318e9cb89d7de21a
MVoght87 <mvoght@uw.edu>
2015-12-05 13:04:07 -0800

    Updated final_project files to match the given code in the assignment description.

--- Final_Project/src/Superblock.java
+++ Final_Project/src/Superblock.java
@@ -1,3 +1,11 @@
 
-public class Superblock {
+class Superblock {
+    public int totalBlocks; // the number of disk blocks
+    public int totalInodes; // the number of inodes
+    public int freeList;    // the block number of the free list's head
+
+    public Superblock( int diskSize ) {
+       // ...
+    }
+    //...
 }
