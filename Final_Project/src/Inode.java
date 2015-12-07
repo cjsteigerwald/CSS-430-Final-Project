8fa806b4b810cf18d215d9a997c9062fd464a94e
cjsteigerwald <cjsteigerwald@gmail.com>
2015-12-06 22:13:33 -0800

    Finish INode class & started Directory class

--- Final_Project/src/Inode.java
+++ Final_Project/src/Inode.java
@@ -2,27 +2,90 @@
 public class Inode {
     private final static int iNodeSize = 32;       // fix to 32 bytes
     private final static int directSize = 11;      // # direct pointers
+    private final static int maxBytes = 512;
+    private final static int blockSize = 16;
+    private final static int intBlock = 4;
+    private final static int shortBlock = 2;
+    private final static int ERROR = -1;
 
-    public int length;                             // file size in bytes
-    public short count;                            // # file-table entries pointing to this
-    public short flag;                             // 0 = unused, 1 = used, ...
+    public int fileSize;                             // file size in bytes
+    public short count;                            // # file-table entries pointing to this, 0 for deletion
+    public short usedFlag;                             // 0 = unused, 1 = used, ...
     public short direct[] = new short[directSize]; // direct pointers
     public short indirect;                         // a indirect pointer
 
     Inode( ) {                                     // a default constructor
-        length = 0;
+        fileSize = 0;
         count = 0;
-        flag = 1;
+        usedFlag = 1;
         for ( int i = 0; i < directSize; i++ )
             direct[i] = -1;
         indirect = -1;
     }
 
-    Inode( short iNumber ) {                       // retrieving inode from disk
+    // 0'th block is reserved for SuperBlock therefore adding 1 skips SuperBlock
+    Inode( short iNumber ) {                       // retrieving iNode from disk
         // design it by yourself.
+        // Block number where inode is located on disk
+        int nodeBlock = (iNumber % blockSize) + 1;
+
+        // buffer to hold data from nodeBlock
+        byte[] dataBuffer = new byte[maxBytes];
+        // take block in from disk and put into dataBuffer
+        SysLib.rawread(nodeBlock, dataBuffer);
+
+        // spaceTracker is used to iterate through the block based on variable thickness
+        int spaceTracker = (iNumber % blockSize) * iNodeSize;
+        // read from dataBuffer into iNode, then iterate thickness variable forward
+        fileSize = SysLib.bytes2int(dataBuffer, spaceTracker);
+        spaceTracker += intBlock;
+        count = SysLib.bytes2short(dataBuffer, spaceTracker);
+        spaceTracker += shortBlock;
+        usedFlag = SysLib.bytes2short(dataBuffer, spaceTracker);
+        spaceTracker += shortBlock;
+        // for loop to iterate through direct pointers
+        for (int i = 0; i < directSize; i++)
+        {
+            direct[i] = SysLib.bytes2short(dataBuffer, spaceTracker);
+            spaceTracker += shortBlock;
+        }
+        indirect = SysLib.bytes2short(dataBuffer, spaceTracker);
     }
 
-    int toDisk( short iNumber ) {                  // save to disk as the i-th inode
+    int toDisk( short iNumber ) {                  // save to disk as the i-th iNode
         // design it by yourself.
+        // nodeBlock is the iNode number * blockSize + 1 (+1 skips SuperBlock) to determine block to write to
+        int nodeBlock = (iNumber % blockSize) + 1;
+
+        byte[] nodeBuffer = new byte[iNodeSize];
+        int spaceTracker = 0;
+        // writing to temp buffer
+        SysLib.int2bytes(fileSize, nodeBuffer, spaceTracker);
+        spaceTracker += intBlock;
+        SysLib.short2bytes(count, nodeBuffer, spaceTracker);
+        spaceTracker += shortBlock;
+        SysLib.short2bytes(usedFlag, nodeBuffer, spaceTracker);
+        spaceTracker += shortBlock;
+                for(int i = 0; i < directSize; i++)
+        {
+            SysLib.short2bytes(direct[i], nodeBuffer, spaceTracker);
+            spaceTracker += shortBlock;
+        }
+        SysLib.short2bytes(indirect, nodeBuffer, spaceTracker);
+
+        // reset spaceTracker for inserting nodeBuffer into dataBuffer
+        spaceTracker = (iNumber % blockSize) * iNodeSize;
+        // nodeBuffer holds iNode
+        byte[] dataBuffer = new byte[iNodeSize];
+        // for holding the entire block from disk for the iNode buffer to be inserted into at the right location
+        byte[] blockBuffer = new byte[maxBytes];
+        SysLib.rawread(nodeBlock, blockBuffer);
+        // due to the inability for rawwrite to copy a subsection of a block, the iNode must be written to the buffer
+        // System.lang arraycopy is used to copy the buffered iNode into the proper location in the buffered block
+        System.arraycopy(nodeBuffer, 0, blockBuffer, spaceTracker, iNodeSize);
+        // overwrites the block in the location where the original buffered block was pulled
+        SysLib.rawwrite(nodeBlock, blockBuffer);
+        // returns the block number where the iNode was inserted
+        return nodeBlock;
     }
 }
--- Final_Project/src/Superblock.java
+++ Final_Project/src/Superblock.java
@@ -1,106 +1,97 @@
-
 class Superblock {
-    private final int defaultInodeBlocks = 64;
     public int totalBlocks; // the number of disk blocks
     public int totalInodes; // the number of inodes
     public int freeList;    // the block number of the free list's head
 
     public Superblock( int diskSize ) {
-       // ...
-        byte[] blockSize = new byte[512];
-        SysLib.rawread(0, blockSize);
-        this.totalBlocks = SysLib.bytes2int(blockSize, 0);
-        this.totalInodes = SysLib.bytes2int(blockSize, 4);
-        this.freeList = SysLib.bytes2int(blockSize, 8);
+        // read data from disk
+        byte[] sbData = new byte[Disk.blockSize];
+        SysLib.rawread(0, sbData);
 
-        if (this.totalBlocks != diskSize || this.totalInodes <= 0 || this.freeList < 2)
-        {
+        totalBlocks = SysLib.bytes2int(sbData, 0);
+        totalInodes = SysLib.bytes2int(sbData, 4);
+        freeList = SysLib.bytes2int(sbData, 8);
+
+        // check disk contents for problems. If problem, format disk
+        if(this.totalBlocks != diskSize || this.totalInodes <= 0 || this.freeList < 2) {
+            SysLib.cerr("Error: Initializing Superblock, formatting disk!");
             this.totalBlocks = diskSize;
-            SysLib.cerr("default format( 64 )\n");
-            this.format();
+            format();
         }
-    } // end SuperBlock (int diskSize)
+    }
 
-    void sync()
-    {
-        byte[] blockSize = new byte[512];
-        SysLib.int2bytes(this.totalBlocks, blockSize, 0);
-        SysLib.int2bytes(this.totalInodes, blockSize, 4);
-        SysLib.int2bytes(this.freeList, blockSize, 8);
-        SysLib.rawwrite(0, blockSize);
-        SysLib.cerr("Superblock synchronized\n");
-    } // end sync()
-
-    // format iNodes and set all flags == 0 and write to disk
-    void format(int num)
-    {
-        this.totalInodes = num;
-
-        // Instantiate iNodes and write to disk
-        for(short i = 0; i < this.totalInodes; i++)
-        {
-            Inode node = new Inode();
-            node.flag = 0;
-            node.toDisk(i);
-        }
-
-        this.freeList = 2 + this.totalInodes * 32 / 512;
-
-        // Instantiate freeList and set all bytes to 0
-        for (int freeBlock = this.freeList; freeBlock < this.totalBlocks; ++freeBlock)
-        {
-            byte[] block = new byte[512];
-
-            for (int blockByte = 0; blockByte < 512; ++blockByte)
-            {
-                block[blockByte] = 0;
-            }
-
-            SysLib.int2bytes(freeBlock + 1, block, 0);
-            SysLib.rawwrite(freeBlock, block);
-        }
-        this.sync();
-    } // end format(int num)
-
-    void format()
-    {
+    // format disk with number of Inodes
+    public synchronized void format() {
         this.format(64);
     }
 
-    public int getFreeBlock()
-    {
-        int freeBlock = this.freeList;
-        if (freeBlock != -1)
-        {
-            byte[] blockSize = new byte[512];
-            SysLib.rawread(freeBlock, blockSize)
-            this.freeList = SysLib.bytes2int(blockSize, 0);
-            SysLib.int2bytes(0, blockSize, 0);
-            SysLib.rawwrite(freeBlock, blockSize);
-        }
-        return freeBlock;
-    } // end getFreeBlock
+    public synchronized void format(int iNodes) {
+        this.totalInodes = iNodes;
 
-    // 
-    public boolean returnBlock(int block)
-    {
-        if (block < 0)
-        {
-            return false;
-        }
-        else
-        {
-            byte[] blockSize = new byte[512];
+        // set freeList pointer to first free block
+        this.freeList = ((this.totalInodes * 32) / Disk.blockSize) + 2;
 
-            for(int blockByte = 0; blockByte < 512; ++blockByte)
-            {
-                blockSize[blockByte] = 0;
+        // write Superblock contents to disk
+        for( int i = this.freeList; i < this.totalBlocks; i++ ) {
+            byte[] data = new byte[Disk.blockSize];
+            // remove data
+            for( int j = 0; j < Disk.blockSize; j++ ) {
+                data[j] = 0;
             }
-            SysLib.int2bytes(this.freeList, blockSize, 0);
-            SysLib.rawwrite(block, blockSize);
-            this.freeList = block;
-            return true;
+
+            SysLib.int2bytes(i + 1, data, 0);
+            SysLib.rawwrite(i, data); // save to disk
         }
-    } // end returnBlock(int block)
-    //...
-} // end class SuperBlock
+
+        this.sync();
+    }
+
+    // grab free block off the free list
+    public int getFreeBlock() {
+        int returnVal = this.freeList;
+
+        // check if block is valid
+        if(this.freeList != -1) {
+            byte[] data = new byte[Disk.blockSize];
+            SysLib.rawread(this.freeList, data);
+
+            // find new block to use
+            this.freeList = SysLib.bytes2int(data, 0);
+            SysLib.int2bytes(0, data, 0);
+            SysLib.rawwrite(this.freeList, data);
+        }
+
+        return returnVal;
+    }
+
+    // put a free block onto the freeList
+    public boolean returnBlock(short blockNum) {
+        if(blockNum < 0) {
+            return false;
+        } else {
+            byte[] data = new byte[Disk.blockSize];
+            for (int i = 0; i < Disk.blockSize; i++) {
+                data[i] = 0;
+            }
+            SysLib.int2bytes(this.freeList, data, 0);
+            SysLib.rawwrite(blockNum, data);
+
+            this.freeList = blockNum;
+        }
+
+        return true;
+    }
+
+    void sync() {
+        // create buffer
+        byte rawData[] = new byte[Disk.blockSize];
+
+        // save Superblock data
+        SysLib.int2bytes(this.totalBlocks, rawData, 0);
+        SysLib.int2bytes(this.totalInodes, rawData, 4);
+        SysLib.int2bytes(this.freeList, rawData, 8);
+
+        // write Superblock data to disk
+        SysLib.rawwrite(0, rawData);
+    }
+}
